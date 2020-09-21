c25b137197dc0ae2c76d1bdc081c4659863e3f6f
Vedad <vedad.haracic@hotmail.com>
2020-09-21 18:46:49 +0200

    Version 2

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const port = process.env.port || 3000;
 app.listen(port, ()=> console.log('listening on 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
-app.use(function(req, res, next) {
-    if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
-        res.redirect('https://' + req.get('Host') + req.url);
-    }
-    else
-        next();
-});
+//app.use(function(req, res, next) {
+    //if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+        //res.redirect('https://' + req.get('Host') + req.url);
+    //}
+    //else
+       // next();
+//});
 
 //const database=new Datastore('database.db');
 //database.loadDatabase();
@@ -32,19 +32,73 @@ app.post('/api',function(request,response){
 
 });
 
-app.get('/weather/:latlon', async (request,response) => {
-	const latlon=request.params.latlon.split(',');
-	console.log(latlon);
-	const lat=latlon[0];
-	const lon=latlon[1];
-	const api_url=`https://api.darksky.net/forecast/9350dca574ba7008610b6219c3ba2f29/${lon},${lat}`;
-    const fetch_response = await fetch(api_url);
-    const json = await fetch_response.json();
-    response.json(json);
-    
-    
-    
+app.get('/weather/Wien', async (request,response) => {
+	
+	//console.log(latlon);
+	//const lat=latlon[0];
+	//const lon=latlon[1];
+	const api_url_W=`https://api.darksky.net/forecast/9350dca574ba7008610b6219c3ba2f29/48.2,16.37`;
+	
+    const fetch_response= await fetch(api_url_W);
+    const json_W = await fetch_response.json();
+    response.json(json_W);
+    console.log("Fetching data for Vienna");
 
+    
+});
+
+
+
+
+
+app.get('/weather/Graz', async (request,response) => {
+	const api_url_G="https://api.darksky.net/forecast/9350dca574ba7008610b6219c3ba2f29/47.07,15.43";
+	const fetch_response = await fetch(api_url_G);
+    const json_G = await fetch_response.json();
+    response.json(json_G);
+    console.log("Fetching data for Graz");
+    
 
 });
 
+app.get('/weather/Linz', async (request,response) => {
+	const api_url_L=`https://api.darksky.net/forecast/9350dca574ba7008610b6219c3ba2f29/48.3,14.28`;
+	const fetch_response = await fetch(api_url_L);
+    const json_L = await fetch_response.json();
+    response.json(json_L);
+    console.log("Fetching data for Linz");
+
+});
+
+app.get('/weather/Villach', async (request,response) => {
+	const api_url_V=`https://api.darksky.net/forecast/9350dca574ba7008610b6219c3ba2f29/46.6,13.85`;
+	const fetch_response = await fetch(api_url_V);
+    const json_V = await fetch_response.json();
+    response.json(json_V);
+    console.log("Fetching data for Villach");
+
+});
+app.get('/weather/Salzburg', async (request,response) => {
+	const api_url_S=`https://api.darksky.net/forecast/9350dca574ba7008610b6219c3ba2f29/47.81,13.03`;
+	const fetch_response = await fetch(api_url_S);
+    const json_S = await fetch_response.json();
+    response.json(json_S);
+    console.log("Fetching data for Salzburg");
+
+});
+app.get('/weather/Innsbruck', async (request,response) => {
+	const api_url_I=`https://api.darksky.net/forecast/9350dca574ba7008610b6219c3ba2f29/47.26,11.39`;
+	const fetch_response = await fetch(api_url_I);
+    const json_I = await fetch_response.json();
+    response.json(json_I);
+    console.log("Fetching data for Inssbruck");
+
+});
+app.get('/weather/Bregenz', async (request,response) => {
+	const api_url_B=`https://api.darksky.net/forecast/9350dca574ba7008610b6219c3ba2f29/47.5,9.73`;
+	const fetch_response = await fetch(api_url_B);
+    const json_B = await fetch_response.json();
+    response.json(json_B);
+    console.log("Fetching data for Bregenz");
+
+});
--- public/sketch.js
+++ public/sketch.js
@@ -37,59 +37,198 @@ AFRAME.registerComponent('cursor-listener', {
   }
 });
 
-if ('geolocation' in navigator) {
-        console.log('geolocation avaivable');
-        navigator.geolocation.getCurrentPosition(async position =>{
-          const lat=position.coords.latitude;
-          const lon=position.coords.longitude;
+
+        
+        
+          //const lat=position.coords.latitude;
+          //const lon=position.coords.longitude;
           //document.getElementById('latitude').setAttribute("text",{value: 'Longitude: '+lat.toFixed(2)});
           //document.getElementById('longitude').setAttribute("text",{value: 'Latitude: '+lon.toFixed(2)});
           
-          const api_url=`weather/${lon},${lat}`;
-          
-          const response= await fetch(api_url);
-          const json_weather=await response.json();
-          
-          const weather_data_today=json_weather.daily.data[0];
-          const weather_data_tommorow=json_weather.daily.data[1];
-          const weather_data_t2=json_weather.daily.data[2];
-          const weather_data_t3=json_weather.daily.data[3];
-          document.getElementById('summary').setAttribute("text",{value:weather_data_today['summary']});
-          document.getElementById('time').setAttribute("text",{value:dayGet(weather_data_today['time'])+', '+timeConverter(weather_data_today['time'])});
-          document.getElementById('maxtemp').setAttribute("text",{value:'Max Temperature: '+toCelsius(weather_data_today['temperatureMax'])+'Celsius'});
-          document.getElementById('mintemp').setAttribute("text",{value:'Min Temperature: '+toCelsius(weather_data_today['temperatureMin'])+' Celsius'});
-          document.getElementById('humidity').setAttribute("text",{value:'Humidity: '+weather_data_today['humidity']*100+' %'});
-          document.getElementById('uvindex').setAttribute("text",{value:'UV-Index: '+weather_data_today['uvIndex']});
-          document.getElementById('location').setAttribute("text",{value:'Location: '+json_weather['timezone']});
-          document.getElementById('percip').setAttribute("text",{value:'Precipitation Probability: '+(weather_data_today['precipProbability']*100).toFixed(0)+' %'});
 
 
-          document.getElementById('summary+1').setAttribute("text",{value:weather_data_tommorow['summary']});
-          document.getElementById('time+1').setAttribute("text",{value:dayGet(weather_data_tommorow['time'])+', '+timeConverter(weather_data_tommorow['time'])});
-          document.getElementById('maxtemp+1').setAttribute("text",{value:'Max Temperature: '+toCelsius(weather_data_tommorow['temperatureMax'])+' Celsius'});
-          document.getElementById('mintemp+1').setAttribute("text",{value:'Min Temperature: '+toCelsius(weather_data_tommorow['temperatureMin'])+' Celsius'});
-          document.getElementById('humidity+1').setAttribute("text",{value:'Humidity: '+weather_data_tommorow['humidity']*100+' %'});
-          document.getElementById('uvindex+1').setAttribute("text",{value:'UV-Index: '+weather_data_tommorow['uvIndex']});
-          document.getElementById('percip+1').setAttribute("text",{value:'Precipitation Probability: '+(weather_data_tommorow['precipProbability']*100).toFixed(0)+' %'});
-          
+async function getWeatherGraz(){
+  const api_url_G ="/weather/Graz";          
+  const response = await fetch(api_url_G);
+  const json_weather_G = await response.json();
+  const weather_data_G=json_weather_G.daily.data[0];
+  console.log(json_weather_G);
+
+  document.getElementById('summary+G').setAttribute("text",{value:weather_data_G['summary']});
+  document.getElementById('time+G').setAttribute("text",{value:dayGet(weather_data_G['time'])+', '+timeConverter(weather_data_G['time'])});
+  document.getElementById('maxtemp+G').setAttribute("text",{value:'Max Temperature: '+toCelsius(weather_data_G['temperatureMax'])+'Celsius'});
+  document.getElementById('mintemp+G').setAttribute("text",{value:'Min Temperature: '+toCelsius(weather_data_G['temperatureMin'])+' Celsius'});
+  document.getElementById('humidity+G').setAttribute("text",{value:'Humidity: '+weather_data_G['humidity']*100+' %'});
+  document.getElementById('uvindex+G').setAttribute("text",{value:'UV-Index: '+weather_data_G['uvIndex']});
+  document.getElementById('location+G').setAttribute("text",{value:'Location: '+' Graz'});
+  document.getElementById('percip+G').setAttribute("text",{value:'Precipitation Probability: '+(weather_data_G['precipProbability']*100).toFixed(0)+' %'});
+
+  
+}
+
+async function getWeatherWien(){
+  const api_url_W ="/weather/Wien";          
+  const response = await fetch(api_url_W);
+  const json_weather_W = await response.json();
+  const weather_data_W=json_weather_W.daily.data[0];
+  console.log(json_weather_W);
+
+  document.getElementById('summary').setAttribute("text",{value:weather_data_W['summary']});
+  document.getElementById('time').setAttribute("text",{value:dayGet(weather_data_W['time'])+', '+timeConverter(weather_data_W['time'])});
+  document.getElementById('maxtemp').setAttribute("text",{value:'Max Temperature: '+toCelsius(weather_data_W['temperatureMax'])+'Celsius'});
+  document.getElementById('mintemp').setAttribute("text",{value:'Min Temperature: '+toCelsius(weather_data_W['temperatureMin'])+' Celsius'});
+  document.getElementById('humidity').setAttribute("text",{value:'Humidity: '+weather_data_W['humidity']*100+' %'});
+  document.getElementById('uvindex').setAttribute("text",{value:'UV-Index: '+weather_data_W['uvIndex']});
+  document.getElementById('location').setAttribute("text",{value:'Location: '+' Vienna'});
+  document.getElementById('percip').setAttribute("text",{value:'Precipitation Probability: '+(weather_data_W['precipProbability']*100).toFixed(0)+' %'});
+
+  
+}              
 
 
-          document.getElementById('summary+2').setAttribute("text",{value:weather_data_t2['summary']});
-          document.getElementById('time+2').setAttribute("text",{value:dayGet(weather_data_t2['time'])+', '+timeConverter(weather_data_t2['time'])});
-          document.getElementById('maxtemp+2').setAttribute("text",{value:'Max Temperature: '+toCelsius(weather_data_t2['temperatureMax'])+' Celsius'});
-          document.getElementById('mintemp+2').setAttribute("text",{value:'Min Temperature: '+toCelsius(weather_data_t2['temperatureMin'])+' Celsius'});
-          document.getElementById('humidity+2').setAttribute("text",{value:'Humidity: '+weather_data_t2['humidity']*100+' %'});
-          document.getElementById('uvindex+2').setAttribute("text",{value:'UV-Index: '+weather_data_t2['uvIndex']});
-          document.getElementById('percip+2').setAttribute("text",{value:'Precipitation Probability: '+(weather_data_t2['precipProbability']*100).toFixed(0)+' %'});
-          
+async function getWeatherLinz(){
+  const api_url_L ="/weather/Linz";          
+  const response = await fetch(api_url_L);
+  const json_weather_L = await response.json();
+  const weather_data_L=json_weather_L.daily.data[0];
+  console.log(json_weather_L);
 
-          document.getElementById('summary+3').setAttribute("text",{value:weather_data_t3['summary']});
-          document.getElementById('time+3').setAttribute("text",{value:dayGet(weather_data_t3['time'])+', '+timeConverter(weather_data_t3['time'])});
-          document.getElementById('maxtemp+3').setAttribute("text",{value:'Max Temperature: '+toCelsius(weather_data_t3['temperatureMax'])+' Celsius'});
-          document.getElementById('mintemp+3').setAttribute("text",{value:'Min Temperature: '+toCelsius(weather_data_t3['temperatureMin'])+' Celsius'});
-          document.getElementById('humidity+3').setAttribute("text",{value:'Humidity: '+weather_data_t3['humidity']*100+' %'});
-          document.getElementById('uvindex+3').setAttribute("text",{value:'UV-Index: '+weather_data_t3['uvIndex']});
-          document.getElementById('percip+3').setAttribute("text",{value:'Precipitation Probability: '+(weather_data_t3['precipProbability']*100).toFixed(0)+' %'});
+  document.getElementById('summary+L').setAttribute("text",{value:weather_data_L['summary']});
+  document.getElementById('time+L').setAttribute("text",{value:dayGet(weather_data_L['time'])+', '+timeConverter(weather_data_L['time'])});
+  document.getElementById('maxtemp+L').setAttribute("text",{value:'Max Temperature: '+toCelsius(weather_data_L['temperatureMax'])+'Celsius'});
+  document.getElementById('mintemp+L').setAttribute("text",{value:'Min Temperature: '+toCelsius(weather_data_L['temperatureMin'])+' Celsius'});
+  document.getElementById('humidity+L').setAttribute("text",{value:'Humidity: '+weather_data_L['humidity']*100+' %'});
+  document.getElementById('uvindex+L').setAttribute("text",{value:'UV-Index: '+weather_data_L['uvIndex']});
+  document.getElementById('location+L').setAttribute("text",{value:'Location: '+' Linz'});
+  document.getElementById('percip+L').setAttribute("text",{value:'Precipitation Probability: '+(weather_data_L['precipProbability']*100).toFixed(0)+' %'});
+
+  
+}              
+
+async function getWeatherSalzburg(){
+  const api_url_S ="/weather/Salzburg";          
+  const response = await fetch(api_url_S);
+  const json_weather_S = await response.json();
+  const weather_data_S=json_weather_S.daily.data[0];
+  console.log(weather_data_S);
+  
+
+  document.getElementById('summary+S').setAttribute("text",{value:weather_data_S['summary']});
+  document.getElementById('time+S').setAttribute("text",{value:dayGet(weather_data_S['time'])+', '+timeConverter(weather_data_S['time'])});
+  document.getElementById('maxtemp+S').setAttribute("text",{value:'Max Temperature: '+toCelsius(weather_data_S['temperatureMax'])+'Celsius'});
+  document.getElementById('mintemp+S').setAttribute("text",{value:'Min Temperature: '+toCelsius(weather_data_S['temperatureMin'])+' Celsius'});
+  document.getElementById('humidity+S').setAttribute("text",{value:'Humidity: '+weather_data_S['humidity']*100+' %'});
+  document.getElementById('uvindex+S').setAttribute("text",{value:'UV-Index: '+weather_data_S['uvIndex']});
+  document.getElementById('location+S').setAttribute("text",{value:'Location: '+' Salzburg'});
+  document.getElementById('percip+S').setAttribute("text",{value:'Precipitation Probability: '+(weather_data_S['precipProbability']*100).toFixed(0)+' %'});
+
+  
+}
+
+async function getWeatherInn(){
+  const api_url_I ="/weather/Innsbruck";          
+  const response = await fetch(api_url_I);
+  const json_weather_I = await response.json();
+  const weather_data_I=json_weather_I.daily.data[0];
+  console.log(json_weather_I);
+
+  document.getElementById('summary+I').setAttribute("text",{value:weather_data_I['summary']});
+  document.getElementById('time+I').setAttribute("text",{value:dayGet(weather_data_I['time'])+', '+timeConverter(weather_data_I['time'])});
+  document.getElementById('maxtemp+I').setAttribute("text",{value:'Max Temperature: '+toCelsius(weather_data_I['temperatureMax'])+'Celsius'});
+  document.getElementById('mintemp+I').setAttribute("text",{value:'Min Temperature: '+toCelsius(weather_data_I['temperatureMin'])+' Celsius'});
+  document.getElementById('humidity+I').setAttribute("text",{value:'Humidity: '+weather_data_I['humidity']*100+' %'});
+  document.getElementById('uvindex+I').setAttribute("text",{value:'UV-Index: '+weather_data_I['uvIndex']});
+  document.getElementById('location+I').setAttribute("text",{value:'Location: '+' Innsbruck'});
+  document.getElementById('percip+I').setAttribute("text",{value:'Precipitation Probability: '+(weather_data_I['precipProbability']*100).toFixed(0)+' %'});
+
+  
+}
+
+async function getWeatherVillach(){
+  const api_url_V ="/weather/Villach";          
+  const response = await fetch(api_url_V);
+  const json_weather_V = await response.json();
+  const weather_data_V=json_weather_V.daily.data[0];
+  console.log(json_weather_V);
+
+  document.getElementById('summary+V').setAttribute("text",{value:weather_data_V['summary']});
+  document.getElementById('time+V').setAttribute("text",{value:dayGet(weather_data_V['time'])+', '+timeConverter(weather_data_V['time'])});
+  document.getElementById('maxtemp+V').setAttribute("text",{value:'Max Temperature: '+toCelsius(weather_data_V['temperatureMax'])+'Celsius'});
+  document.getElementById('mintemp+V').setAttribute("text",{value:'Min Temperature: '+toCelsius(weather_data_V['temperatureMin'])+' Celsius'});
+  document.getElementById('humidity+V').setAttribute("text",{value:'Humidity: '+weather_data_V['humidity']*100+' %'});
+  document.getElementById('uvindex+V').setAttribute("text",{value:'UV-Index: '+weather_data_V['uvIndex']});
+  document.getElementById('location+V').setAttribute("text",{value:'Location: '+' Villach'});
+  document.getElementById('percip+V').setAttribute("text",{value:'Precipitation Probability: '+(weather_data_V['precipProbability']*100).toFixed(0)+' %'});
+
+  
+}
+
+async function getWeatherBregenz(){
+  const api_url_B ="/weather/Bregenz";          
+  const response = await fetch(api_url_B);
+  const json_weather_B = await response.json();
+  const weather_data_B=json_weather_B.daily.data[0];
+  console.log(json_weather_B);
+
+  document.getElementById('summary+B').setAttribute("text",{value:weather_data_B['summary']});
+  document.getElementById('time+B').setAttribute("text",{value:dayGet(weather_data_B['time'])+', '+timeConverter(weather_data_B['time'])});
+  document.getElementById('maxtemp+B').setAttribute("text",{value:'Max Temperature: '+toCelsius(weather_data_B['temperatureMax'])+'Celsius'});
+  document.getElementById('mintemp+B').setAttribute("text",{value:'Min Temperature: '+toCelsius(weather_data_B['temperatureMin'])+' Celsius'});
+  document.getElementById('humidity+B').setAttribute("text",{value:'Humidity: '+weather_data_B['humidity']*100+' %'});
+  document.getElementById('uvindex+B').setAttribute("text",{value:'UV-Index: '+weather_data_B['uvIndex']});
+  document.getElementById('location+B').setAttribute("text",{value:'Location: '+' Bregenz'});
+  document.getElementById('percip+B').setAttribute("text",{value:'Precipitation Probability: '+(weather_data_B['precipProbability']*100).toFixed(0)+' %'});
+
+  
+}          
+
+
+////const response_W= fetch(api_url_W);
+////console.log(response_W);
+////const json_weather_W= response_W.json();
+////console.log(json_weather_W);
+
+////const response_L= fetch(api_url_L);
+////console.log(response_L);
+////const json_weather_L= response_L.json();
+////console.log(json_weather_L);
+
+//const response_V= fetch(api_url_V);
+//console.log(response_V);
+//const json_weather_V= response_V.json();
+//console.log(json_weather_V);
+
+
+
+getWeatherGraz();
+getWeatherWien();
+getWeatherLinz();
+getWeatherSalzburg();
+getWeatherVillach();
+getWeatherBregenz();
+getWeatherInn();
+
+
+////const weather_data_W=json_weather_W.daily.data[0];
+//const weather_data_G=json_weather_G.daily.data[0];
+////const weather_data_L=json_weather_L.daily.data[0];
+////const weather_data_V=json_weather_V.daily.data[0];
+////const weather_data_S=json_weather_S.daily.data[0];
+////const weather_data_I=json_weather_I.daily.data[0];
+////const weather_data_B=json_weather_B.daily.data[0];
+
+
+//document.getElementById('summary').setAttribute("text",{value:weather_data_G['summary']});
+//document.getElementById('time').setAttribute("text",{value:dayGet(weather_data_G['time'])+', '+timeConverter(weather_data_G['time'])});
+//document.getElementById('maxtemp').setAttribute("text",{value:'Max Temperature: '+toCelsius(weather_data_G['temperatureMax'])+'Celsius'});
+//document.getElementById('mintemp').setAttribute("text",{value:'Min Temperature: '+toCelsius(weather_data_G['temperatureMin'])+' Celsius'});
+//document.getElementById('humidity').setAttribute("text",{value:'Humidity: '+weather_data_G['humidity']*100+' %'});
+//document.getElementById('uvindex').setAttribute("text",{value:'UV-Index: '+weather_data_G['uvIndex']});
+//document.getElementById('location').setAttribute("text",{value:'Location: '+json_weather['timezone']});
+//document.getElementById('percip').setAttribute("text",{value:'Precipitation Probability: '+(weather_data_G['precipProbability']*100).toFixed(0)+' %'});
+
+
+
 
 
           //document.querySelector('#cameraWrapper').setAttribute('position', {x:5, y:1.6, z:9});
@@ -111,11 +250,9 @@ if ('geolocation' in navigator) {
           //const json=await response.json();
           //console.log(json);
             
-        });
+        
 
-      }else {
-        console.log('geolocation not avaivable');
-      }
+      
 
 
 
